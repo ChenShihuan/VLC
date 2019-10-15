6c78e5179f3e7f86975bd283b9edcacc3b7096ba
ChenShihuan <cnchensh@outlook.com>
2019-10-15 20:09:50 +0800

    更新双灯定位程序，更新三灯定位程序输入变量名称

--- vlc_locator/src/positioningCalculation.cpp
+++ vlc_locator/src/positioningCalculation.cpp
@@ -12,7 +12,7 @@
 //-----------------------------------------------------------------------------------------------
 
 
-struct XYZ double_LED(double f,double Center_X, double Center_Y, struct LED A,struct LED B)
+struct XYZ double_LED(double f,double Center_X, double Center_Y, struct LED D1,struct LED D2)
 {
 	double ImgX1;
 	double ImgY1;
@@ -23,41 +23,139 @@ struct XYZ double_LED(double f,double Center_X, double Center_Y, struct LED A,st
 	double x2;
 	double y2;
 
-	if (A.Y>B.Y){
-		ImgX1 = A.imgLocalX;
-		ImgY1 = A.imgLocalY;
-		ImgX2 = B.imgLocalX;
-		ImgY2 = B.imgLocalY;
-		x1 = A.X;
-		y1 = A.Y;
-		x2 = B.X;
-		y2 = B.Y;
-	}
 
-	else
-	{
-		ImgX1 = B.imgLocalX;
-		ImgY1 = B.imgLocalY;
-		ImgX2 = A.imgLocalX;
-		ImgY2 = A.imgLocalY;
-		x1 = B.X;
-		y1 = B.Y;
-		x2 = A.X;
-		y2 = A.Y;
-	}
-
+	// cout << "D1="<< D1.ID << '\n';
+	// cout << "D2="<< D2.ID << '\n';
+	// 计算角度
 	double alpha;
+	if (D1.X == D2.X ){
+		if (D1.Y<D2.Y){
+			ImgX1 = D1.imgLocalX;
+			ImgY1 = D1.imgLocalY;
+			ImgX2 = D2.imgLocalX;
+			ImgY2 = D2.imgLocalY;
+			x1 = D1.X;
+			y1 = D1.Y;
+			x2 = D2.X;
+			y2 = D2.Y;
+		}
 
-	if (x1>x2){
-		alpha = -(3*pi/4);
+		else
+		{
+			ImgX1 = D2.imgLocalX;
+			ImgY1 = D2.imgLocalY;
+			ImgX2 = D1.imgLocalX;
+			ImgY2 = D1.imgLocalY;
+			x1 = D2.X;
+			y1 = D2.Y;
+			x2 = D1.X;
+			y2 = D1.Y;
+		}
+		alpha = (pi/2);
+		// if (y1<y2){
+		// 	alpha = (pi/4)+(pi/4);
+		// }
+
+		// else{
+		// 	alpha = (3*pi/4)-(pi/4);
+		// }
+
+	}
+	else if (D1.Y == D2.Y){
+		if (D1.X<D2.X){
+			ImgX1 = D1.imgLocalX;
+			ImgY1 = D1.imgLocalY;
+			ImgX2 = D2.imgLocalX;
+			ImgY2 = D2.imgLocalY;
+			x1 = D1.X;
+			y1 = D1.Y;
+			x2 = D2.X;
+			y2 = D2.Y;
+		}
+
+		else
+		{
+			ImgX1 = D2.imgLocalX;
+			ImgY1 = D2.imgLocalY;
+			ImgX2 = D1.imgLocalX;
+			ImgY2 = D1.imgLocalY;
+			x1 = D2.X;
+			y1 = D2.Y;
+			x2 = D1.X;
+			y2 = D1.Y;
+		}
+		alpha = 0;
+		// if (x1<x2){
+		// 	alpha = (pi/4)-(pi/4);
+		// }
+
+		// else{
+		// 	alpha = (3*pi/4)+(pi/4);
+		// }
+
+	}
+	else{
+		if (D1.X<D2.X){
+			ImgX1 = D1.imgLocalX;
+			ImgY1 = D1.imgLocalY;
+			ImgX2 = D2.imgLocalX;
+			ImgY2 = D2.imgLocalY;
+			x1 = D1.X;
+			y1 = D1.Y;
+			x2 = D2.X;
+			y2 = D2.Y;
+		}
+
+		else
+		{
+			ImgX1 = D2.imgLocalX;
+			ImgY1 = D2.imgLocalY;
+			ImgX2 = D1.imgLocalX;
+			ImgY2 = D1.imgLocalY;
+			x1 = D2.X;
+			y1 = D2.Y;
+			x2 = D1.X;
+			y2 = D1.Y;
+		}
+	
+		alpha = atan((D2.Y - D1.Y) / (D2.X - D1.X));
+
+		// cout << "alpha=" << alpha / pi * 180 << '\n';
 	}
 
-	else
+
+	double angle;
+	if (ImgX2 == ImgX1){
+		angle = (pi/2);
+	}
+	else{
+		angle = atan((ImgY2 - ImgY1) / (ImgX2 - ImgX1));
+	}
+	
+	// cout << "angle1=" << angle / pi * 180 << '\n';
+
+	//由于对称性，要对角度做进一步处理
+	bool ABC = ImgY2 < ImgY1;
+	bool EFG = ImgX2 > ImgX1;
+	int ABCD = ABC * 2 + EFG;
+	//ABCD = 3;
+	// cout << "ABCD=" << ABCD << '\n';
+
+	switch (ABCD)
 	{
-		alpha = -(pi/4);
+	case 0:
+		angle = - angle + alpha;
+		break;
+	case 1:
+		angle = pi - angle + alpha;
+		break;
+	case 2:
+		angle = - angle + alpha;
+		break;
+	case 3:
+		angle = pi - angle + alpha;
+		break;
 	}
-	cout << "alpha=" << alpha << '\n';
-
 
 	double d_12 = sqrt(pow((ImgX1 - ImgX2),2) + pow((ImgY1 - ImgY2),2))*3.2e-3;
 	double D_12 = sqrt(pow((x1 - x2),2) + pow((y1 - y2),2));
@@ -69,40 +167,6 @@ struct XYZ double_LED(double f,double Center_X, double Center_Y, struct LED A,st
 	double X = X_r;
 	double Y = Y_r;
 
-	// cout << "H=" << H << '\n';
-
-	// 计算角度
-	// cout << "ImgY2=" << ImgY2 << '\n';
-	// cout << "ImgY1=" << ImgY1 << '\n';
-	// cout << "ImgX2 =" << ImgX2 << '\n';
-	// cout << "ImgX1=" << ImgX1 << '\n';
-	double K1 = abs((ImgY2 - ImgY1) / (ImgX2 - ImgX1));
-	// cout << "K1=" << K1  << '\n';
-	double angle = atan(K1);
-	// cout << "angle1=" << angle / pi * 180 << '\n';
-
-	//由于对称性，要对角度做进一步处理
-	bool ABC = ImgY2 < ImgY1;
-	bool EFG = ImgX2 > ImgX1;
-	int ABCD = ABC * 2 + EFG;
-	// ABCD = 3;
-	// cout << "ABCD=" << ABCD << '\n';
-
-	switch (ABCD)
-	{
-	case 0:
-		angle = angle + alpha;
-		break;
-	case 1:
-		angle = pi - angle + alpha;
-		break;
-	case 2:
-		angle = 2 * pi - angle + alpha;
-		break;
-	case 3:
-		angle = angle + pi + alpha;
-		break;
-	}
 	// cout << "angle=" << angle / pi * 180 << '\n';
 		
 	double XX = X*cos(angle) - Y*sin(angle);
@@ -116,7 +180,6 @@ struct XYZ double_LED(double f,double Center_X, double Center_Y, struct LED A,st
 	double zz = 150 - H / 10;
 
 	// imshow("test time", grayImage);
-
 	struct XYZ pose;
 	pose.x=xx;
 	pose.y=yy;
@@ -125,20 +188,20 @@ struct XYZ double_LED(double f,double Center_X, double Center_Y, struct LED A,st
 
 }
 
-struct XYZ three_LED(double f, double Center_X, double Center_Y, struct LED A,struct LED B,struct LED C)
+struct XYZ three_LED(double f, double Center_X, double Center_Y, struct LED D1,struct LED D2, struct LED D3)
 {
-	double ImgX1 = A.imgLocalX;
-	double ImgY1 = A.imgLocalY;
-	double ImgX2 = B.imgLocalX;
-	double ImgY2 = B.imgLocalY;
-	double ImgX3 = C.imgLocalX;
-	double ImgY3 = C.imgLocalY;
-	double x1 = A.X;
-	double y1 = A.Y;
-	double x2 = B.X;
-	double y2 = B.Y;
-	double x3 = C.X;
-	double y3 = C.Y;
+	double ImgX1 = D1.imgLocalX;
+	double ImgY1 = D1.imgLocalY;
+	double ImgX2 = D2.imgLocalX;
+	double ImgY2 = D2.imgLocalY;
+	double ImgX3 = D3.imgLocalX;
+	double ImgY3 = D3.imgLocalY;
+	double x1 = D1.X;
+	double y1 = D1.Y;
+	double x2 = D2.X;
+	double y2 = D2.Y;
+	double x3 = D3.X;
+	double y3 = D3.Y;
 
 	//三灯定位
 	double d_12 = sqrt(pow((ImgX1 - ImgX2), 2) + pow((ImgY1 - ImgY2), 2))*3.2e-3;
@@ -164,10 +227,6 @@ struct XYZ three_LED(double f, double Center_X, double Center_Y, struct LED A,st
 	double r2 = pow(D_2, 2);
 	double r3 = pow(D_3, 2);
 
-	// double rr1 = pow(d_1, 2);
-	// double rr2 = pow(d_2, 2);
-	// double rr3 = pow(d_3, 2);
-
 	//解出终端的位置坐标
 	double a1 = 2 * (x1 - x3);
 	double b1 = 2 * (y1 - y3);
@@ -175,13 +234,6 @@ struct XYZ three_LED(double f, double Center_X, double Center_Y, struct LED A,st
 	double a2 = 2 * (x2 - x3);
 	double b2 = 2 * (y2 - y3);
 	double c2 = pow(x3, 2) - pow(x2, 2) + pow(y3, 2) - pow(y2, 2) - r3 + r2;
-
-	// double a1 = 2 * (ImgX1 - ImgX3);
-	// double b1 = 2 * (ImgY1 - ImgY3);
-	// double c1 = pow(ImgX3, 2) - pow(ImgX1, 2) + pow(ImgY3, 2) - pow(ImgY1, 2) - rr3 + rr1;
-	// double a2 = 2 * (ImgX2 - ImgX3);
-	// double b2 = 2 * (ImgY2 - ImgY3);
-	// double c2 = pow(ImgX3, 2) - pow(ImgX2, 2) + pow(ImgY3, 2) - pow(ImgY2, 2) - rr3 + rr2;
 
 	double XX = (c2 * b1 - c1 * b2) / (a1*b2 - a2 * b1);
 	double YY = (c2 * a1 - c1 * a2) / (a2*b1 - a1 * b2);
